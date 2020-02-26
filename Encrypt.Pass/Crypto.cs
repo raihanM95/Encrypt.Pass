2957bb6f265067077e17e0c252d8f3ca15daba72
raihanM95 <mahmud.raihan@yahoo.com>
2020-02-26 16:01:14 +0600

    v 1.0.0 & 1.0.1

---
+++ Encrypt.Pass/Crypto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+
+namespace Encrypt.Pass
+{
+    public static class Crypto
+    {
+        public static string Hash(string value)
+        {
+            return Convert.ToBase64String(
+                System.Security.Cryptography.SHA256.Create().ComputeHash(Encoding.UTF8.GetBytes(value))
+            );
+        }
+    }
+}
